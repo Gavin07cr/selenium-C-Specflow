d772dfc933b8820dbe2ae696ccd40014efc4c655
Gavin07cr <107991869+Gavin07cr@users.noreply.github.com>
2022-10-25 10:20:16 +0100

    Add files via upload

---
+++ SpecFlow.Selenium/PageObjects/PageObject.cs
@@ -0,0 +1,36 @@
+﻿using DocumentFormat.OpenXml.Bibliography;
+using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using Xunit;
+
+namespace SpecFlow.Selenium.PageObjects
+{
+    public abstract class PageObject
+    {
+        private readonly IWebDriver _driver;
+
+        public PageObject(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void Navigate(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+            System.Threading.Thread.Sleep(2000);
+            IWebElement firstCheckBox;
+            if (url.Contains("google"))
+                firstCheckBox = _driver.FindElement(OpenQA.Selenium.By.Id("L2AGLb"));
+            else
+                firstCheckBox = _driver.FindElement(By.ClassName(" css-47sehv"));
+            firstCheckBox.Click();
+        }
+
+        public void AssertTitle(string title)
+        {
+            string pageTitle = _driver.Title;
+            pageTitle.Should().Be(title);
+        }
+    }
+}
